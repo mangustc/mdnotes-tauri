388e4e319c0a7e8d8d24e4689017ac4b6db13584
Ivan Lifanov <letalbark@gmail.com>
2026-06-16 13:13:48 +0700

    add linux fixes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        std::env::set_var("GIO_USE_PROXY_RESOLVER", "dummy");
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+        std::env::set_var("__NV_DISABLE_EXPLICIT_SYNC", "1");
+    }
+
     mdnotes_tauri_lib::run()
 }
