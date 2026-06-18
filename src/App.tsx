a28d71e5ecd560dc7ac3401208b03fd7d50fb9d0
Ivan Lifanov <letalbark@gmail.com>
2026-06-19 01:16:31 +0700

    remove @material/web and replace it with beercss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { createSignal } from "solid-js";
 import { invoke } from "@tauri-apps/api/core";
-import "./App.scss";
-import "@material/web/all";
 import { getCurrentWindow } from "@tauri-apps/api/window";
+import "beercss";
+import "material-dynamic-colors";
+import "./App.scss";
 
 function App() {
   const [greetMsg, setGreetMsg] = createSignal("");
@@ -14,46 +15,39 @@ function App() {
   }
 
   return (
-    <main class="app-container">
-      <header data-tauri-drag-region class="desktop-app-bar">
-        <span>My Notes</span>
-        <div>
-          <md-icon-button onClick={() => appWindow.minimize()}>
-            <md-icon>remove</md-icon>
-          </md-icon-button>
-          <md-icon-button onClick={() => appWindow.toggleMaximize()}>
-            <md-icon>check_box_outline_blank</md-icon>
-          </md-icon-button>
-          <md-icon-button onClick={() => appWindow.close()}>
-            <md-icon>close</md-icon>
-          </md-icon-button>
-        </div>
+    <>
+      <header class="top">
+        <nav data-tauri-drag-region>
+          <button class="circle transparent">
+            <i>menu</i>
+          </button>
+          <div class="max" />
+          <button
+            class="circle transparent"
+            onClick={() => appWindow.minimize()}
+          >
+            <i>remove</i>
+          </button>
+          <button
+            class="circle transparent"
+            onClick={() => appWindow.toggleMaximize()}
+          >
+            <i>check_box_outline_blank</i>
+          </button>
+          <button class="circle transparent" onClick={() => appWindow.close()}>
+            <i>close</i>
+          </button>
+        </nav>
       </header>
-      <div class="app-drawer">
-        <div></div>
-        <div class="app-drawer-actions">
-          <md-divider></md-divider>
-          <md-filled-tonal-button style="width: 100%;">
-            Create new note
-            <md-icon slot="icon">create</md-icon>
-          </md-filled-tonal-button>
-          <div class="app-drawer-actions-row">
-            <div class="app-drawer-actions-btn-group">
-              <md-filled-tonal-button>
-                Select project
-                <md-icon slot="icon">folder_open</md-icon>
-              </md-filled-tonal-button>
-              <md-filled-tonal-icon-button>
-                <md-icon>settings</md-icon>
-              </md-filled-tonal-icon-button>
-            </div>
-            <md-outlined-icon-button>
-              <md-icon>sync</md-icon>
-            </md-outlined-icon-button>
-          </div>
+      <main class="responsive" style="display: flex; flex-direction: row">
+        <div>
+          <h3>drawer</h3>
         </div>
-      </div>
-    </main>
+        <div>
+          <h3>main content</h3>
+        </div>
+      </main>
+    </>
   );
 }
 
