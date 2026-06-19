afb1b5f76005dc6aa7883c03269f9617e5f4d53e
Ivan Lifanov <letalbark@gmail.com>
2026-06-20 02:00:07 +0700

    style drawer a little bit

--- src/App.tsx
+++ src/App.tsx
@@ -39,9 +39,32 @@ function App() {
           </button>
         </nav>
       </header>
-      <main class="responsive" style="display: flex; flex-direction: row">
-        <div>
-          <h3>drawer</h3>
+      <main style="display: flex; flex-direction: row; width: 100vw; height: 100%; padding: 0;">
+        <div style="display: flex; flex-direction: column; justify-content: space-between; width: 360px; height: 100%;">
+          <h3>search element</h3>
+          <div>
+            <hr />
+            <div style="padding: 16px">
+              <button class="tetriary responsive">
+                <i>create</i>
+                Create new note
+              </button>
+            </div>
+            <div style="display: flex; flex-direction: row; justify-content: space-between; padding: 0px 16px 16px 16px">
+              <nav class="group split">
+                <button class="left-round">
+                  <i class="small">folder_open</i>
+                  <span>project_name</span>
+                </button>
+                <button class="right-round square">
+                  <i class="small fill">settings</i>
+                </button>
+              </nav>
+              <button class="border square round extra">
+                <i>sync</i>
+              </button>
+            </div>
+          </div>
         </div>
         <div>
           <h3>main content</h3>
